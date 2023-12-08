ec1e218d5331321957dda8172d42693004df0dc4
ndelanhese <nnathanh3@gmail.com>
2023-12-08 16:27:20 -0300

    chore(eslint)add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,9 +6,11 @@ app.get('/ping', () => {
   return 'pong'
 })
 
-app.listen({
-  // port: process.env.APP_PORT ?? 3333
-  port: 3333
-}).then(() => {
-console.log('HTTP Server Running!');
-})
+app
+  .listen({
+    // port: process.env.APP_PORT ?? 3333
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
