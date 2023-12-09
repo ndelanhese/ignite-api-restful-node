837a9401d978446d48a5b9ad94cd2d63f5d7944c
ndelanhese <nnathanh3@gmail.com>
2023-12-09 11:39:51 -0300

    feat(transactions): add list transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,9 +5,26 @@ import { randomUUID } from 'node:crypto'
 
 export const transactionsRoutes = async (app: FastifyInstance) => {
   app.get('/', async () => {
-    const transactions = await knex('transactions').select('*')
+    const transactions = await knex('transactions').select()
 
-    return transactions
+    return {
+      data: transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .select()
+      .where('id', id)
+      .first()
+
+    return transaction
   })
 
   app.post('/', async (request, response) => {
